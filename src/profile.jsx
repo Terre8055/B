782b6d380fba719d4019617b342c7b5e3e297197
Terre8055 <michaelappiah2018@icloud.com>
2023-01-10 17:18:35 +0000

    Moved to build folder

--- src/profile.jsx
+++ src/profile.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Profile() {
     return (
         <header className="profile--picture">
-            <img src="src/assets/Abena.jpeg" alt="prof-pic" />
+            <img src="/src/Abena.jpeg" alt="prof-pic" />
         </header>
     )
 }
