7f5a9a22eff12eece005ce76f49978177cb2bd13
Terre8055 <michaelappiah2018@icloud.com>
2023-01-10 17:36:30 +0000

    Moved image to public directory

--- src/profile.jsx
+++ src/profile.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Profile() {
     return (
         <header className="profile--picture">
-            <img src="/src/Abena.jpeg" alt="prof-pic" />
+            <img src="./Abena.jpeg" alt="prof-pic" />
         </header>
     )
 }
